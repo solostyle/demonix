5fbca6ad0f29dbf5b25c22c5d52991e791fa0f38
solostyle <spam@solostyle.net>
2012-04-14 13:42:33 -0500

    prototype: double number slam

---
+++ prototypes/ReportSummary_files/double-number-slam.js
@@ -0,0 +1,9 @@
+jQuery('#facetsBothHeader').click(function() {
+	jQuery('#facetsBoth').toggle();
+});
+jQuery('#facetsSupplyHeader').click(function() {
+	jQuery('#facetsSupply').toggle();
+});
+jQuery('#facetsDemandHeader').click(function() {
+	jQuery('#facetsDemand').toggle();
+});
