bfcc1f1af1fae00b9ee0762db3217e7f0c7fd819
solostyle <spam@solostyle.net>
2012-04-18 15:53:30 -0500

    prototype1: double number slam. applied filters

--- prototypes/1_files/double-number-slam.js
+++ prototypes/1_files/double-number-slam.js
@@ -21,6 +21,27 @@ jQuery('#facetsDemandHeader').click(function() {
 
 jQuery(".facets li li").click(function() {
 	jQuery(this).toggleClass("selected");
+	jQuery("#searched-for").removeClass("inactive");
+	
+	var slctdWords = jQuery(this).find(".filterCheckboxWrapper").html();
+	var searchFor = "<li><span>"+slctdWords+"</span></li>";
+	var found = '';
+	
+	jQuery("#searched-for .searched-for-content ul li").each(function(i, el) {
+		if (jQuery(el).html()=="<span>"+slctdWords+"</span>") {
+			found = jQuery(el);
+		}
+	});
+	
+	if (found) {
+		// if element exists, remove it
+		found.remove();
+		if (jQuery("#searched-for .searched-for-content ul").html()=="") {
+			jQuery("#searched-for").addClass("inactive");
+		}
+	} else {
+		jQuery("#searched-for .searched-for-content ul").append(searchFor);
+	}
 });
 
 // Close to begin with
