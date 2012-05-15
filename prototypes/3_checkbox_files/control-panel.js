8cdbe67bec0887459c26ae3d8497233e20ad1f3a
solostyle <spam@solostyle.net>
2012-05-15 14:45:31 -0500

    part of previous commit

--- prototypes/3_checkbox_files/control-panel.js
+++ prototypes/3_checkbox_files/control-panel.js
@@ -1,8 +1,10 @@
 jQuery('body')
 .append('<div id="controlpanel" style="position:fixed;top:0;right:0;background-color:rgba(250,250,250,.5)"></div>')
 .find('#controlpanel')
-.append('<p><input type="checkbox" id="numOnRight"><label for="numOnRight">Numbers on right</label></p>')
-.append('<p><input type="checkbox" id="noParens"><label for="noParens">No parentheses</label></p>');
+.append('<ul style="padding:0;list-style:none"></ul>')
+.find('ul')
+.append('<li><input type="checkbox" id="numOnRight"><label for="numOnRight">Numbers on right</label></li>')
+.append('<li><input type="checkbox" id="noParens"><label for="noParens">No parentheses</label></li>');
 
 jQuery('#numOnRight').click( function moveNumbersToRight() {
 	if (jQuery('#numOnRight').attr('checked') == 'checked') {
@@ -14,21 +16,27 @@ jQuery('#numOnRight').click( function moveNumbersToRight() {
 			}
 		});
 	} else {
-		jQuery('.facetCountSupplyWrapper').each(function() {
-			jQuery(this).insertBefore(jQuery(this).prev('.filterCheckboxWrapper'));
-			if (jQuery(this).next().next('.facetCountDemandWrapper').length > 0) {
-				jQuery(this).find('span').html( jQuery(this).find('span').html() + ')' );
-				jQuery(this).next().next('.facetCountDemandWrapper').find('span').html( '(' + jQuery(this).next().next('.facetCountDemandWrapper').find('span').html() );
-			}
-		});
+		if (jQuery('#noParens').attr('checked') == 'checked') {
+			jQuery('.facetCountSupplyWrapper').each(function() {
+				jQuery(this).insertBefore(jQuery(this).prev('.filterCheckboxWrapper'));
+			});
+		} else {
+			jQuery('.facetCountSupplyWrapper').each(function() {
+				jQuery(this).insertBefore(jQuery(this).prev('.filterCheckboxWrapper'));
+				if (jQuery(this).next().next('.facetCountDemandWrapper').length > 0) {
+					jQuery(this).find('span').html( jQuery(this).find('span').html() + ')' );
+					jQuery(this).next().next('.facetCountDemandWrapper').find('span').html( '(' + jQuery(this).next().next('.facetCountDemandWrapper').find('span').html() );
+				}
+			});
+		}
 	}
 });
 
 jQuery('#noParens').click( function removeParens() {
 	if (jQuery('#noParens').attr('checked') == 'checked') {
 		jQuery('.facetCountSupply, .facetCountDemand').each(function() {
-			jQuery(this).html( jQuery(this).html().replace("(", "", "g") );
-			jQuery(this).html( jQuery(this).html().replace(")", "", "g") );
+			jQuery(this).html( jQuery(this).html().replace(/[\(\)]/g, "") );
+			//jQuery(this).html( jQuery(this).html().replace(")", "", "g") );
 		});
 	} else {
 		if (jQuery('#numOnRight').attr('checked') == 'checked') {
