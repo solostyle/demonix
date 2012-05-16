8143053154f90399ad6a2dc9aac6263b26460ac2
solostyle <spam@solostyle.net>
2012-05-16 12:03:21 -0500

    prototype: checkbox. controlpanel behavior

--- prototypes/3_checkbox_files/control-panel.js
+++ prototypes/3_checkbox_files/control-panel.js
@@ -1,13 +1,15 @@
 jQuery('body')
-.append('<div id="controlpanel" style="position:fixed;top:0;right:0;background-color:rgba(250,250,250,.8)"></div>')
+.append('<div id="controlpanel" style="position:fixed;top:0;right:0;background-color:rgba(250,250,250,.7);border-radius:7px;font:normal normal 12px/16px arial,sans-serif"></div>')
 .find('#controlpanel')
-.append('<ul style="padding:0;list-style:none"></ul>')
+.append('<ul style="padding:0;margin:5px;list-style:none"></ul>')
 .find('ul')
 .append('<li><input type="checkbox" id="numOnRight"><label for="numOnRight">Numbers on right</label></li>')
-.append('<li><input type="checkbox" id="noParens"><label for="noParens">No parentheses</label></li>');
+.append('<li><input type="checkbox" id="noParens"><label for="noParens">No parentheses</label></li>')
+.append('<li><input type="checkbox" id="chkbx" checked="checked"><label for="chkbx">Checkbox</label></li>');
+
 
 jQuery('#numOnRight').click( function moveNumbersToRight() {
-	if (jQuery('#numOnRight').attr('checked') == 'checked') {
+	if (jQuery(this).filter(':checked').length) {
 		jQuery('.facetCountSupplyWrapper').each(function() {
 			jQuery(this).insertAfter(jQuery(this).next('.filterCheckboxWrapper'));
 			if (jQuery(this).next('.facetCountDemandWrapper').length > 0) {
@@ -33,7 +35,7 @@ jQuery('#numOnRight').click( function moveNumbersToRight() {
 });
 
 jQuery('#noParens').click( function removeParens() {
-	if (jQuery('#noParens').attr('checked') == 'checked') {
+	if (jQuery(this).filter(':checked').length) {
 		jQuery('.facetCountSupply, .facetCountDemand').each(function() {
 			jQuery(this).html( jQuery(this).html().replace(/[\(\)]/g, "") );
 			//jQuery(this).html( jQuery(this).html().replace(")", "", "g") );
@@ -60,5 +62,13 @@ jQuery('#noParens').click( function removeParens() {
 				jQuery(this).find('span').html( jQuery(this).find('span').html().replace(/^([0-9,]+)$/g, '($1)') );
 			});
 		}
+	}
+});
+
+jQuery('#chkbx').click( function addCheckbox() {
+	if (jQuery(this).filter(':checked').length) {
+	} else {
+		// remove checkboxes
+		jQuery('#AllFilters .facetCheckbox').remove();
 	}
 });
--- prototypes/3_checkbox_files/load-chkbx-html.js
+++ prototypes/3_checkbox_files/load-chkbx-html.js
@@ -1,26 +1,11 @@
-var facetsObject = {
-	facetsBoth: {
-		id:'#facetsBoth',
-		text:{}
-	}, 
-	facetsSupply: {
-		id:'#facetsSupply',
-		text:[]
-	}, 
-	facetsDemand: {
-		id:'#facetsDemand',
-		text:{}
-	}
-};
-
 jQuery('#facetsSupply .facets>li>div:last-child ul, #facetsBoth .facets>li>div:last-child ul, #facetsDemand .facets>li>div:last-child ul').each(function insert() {
 	var facet = jQuery(this).parent().attr('id'),
-	section = jQuery(this).parent().parent().parent().parent().parent().attr('id').split('facets')[1];
+	section = jQuery(this).parents().eq(4).attr('id').split('facets')[1];
 	
 	jQuery(this)
 	.children()
 	.each(function () {
-		jQuery(this).prepend('<div class="facetCheckbox"><input id="chkbx'+'_'+facet+'_'+jQuery(this).find('.filterCheckboxWrapper').html().replace(/\s/g,"")+'_'+section+'" type="checkbox" /></div>');
+		jQuery(this).prepend('<div class="facetCheckbox"><input id="chkbx_'+facet+'_'+jQuery(this).find('.filterCheckboxWrapper').html().replace(/\s/g,"")+'_'+section+'" type="checkbox" /></div>');
 	});
 });
 	
