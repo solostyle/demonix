d73c54cfb62d1fae8ebf96b051ca8693eed74e22
solostyle <spam@solostyle.net>
2012-05-16 14:34:54 -0500

    prototypes: checkbox on off option, with width change

--- prototypes/3_checkbox_files/control-panel.js
+++ prototypes/3_checkbox_files/control-panel.js
@@ -3,8 +3,8 @@ jQuery('body')
 .find('#controlpanel')
 .append('<ul style="padding:0;margin:5px;list-style:none"></ul>')
 .find('ul')
-.append('<li><input type="checkbox" id="numOnRight"><label for="numOnRight">Numbers on right</label></li>')
-.append('<li><input type="checkbox" id="noParens"><label for="noParens">No parentheses</label></li>')
+.append('<li><input type="checkbox" id="numOnRight" checked="checked"><label for="numOnRight">Numbers on right</label></li>')
+.append('<li><input type="checkbox" id="noParens" checked="checked"><label for="noParens">No parentheses</label></li>')
 .append('<li><input type="checkbox" id="chkbx" checked="checked"><label for="chkbx">Checkbox</label></li>');
 
 
@@ -18,7 +18,7 @@ jQuery('#numOnRight').click( function moveNumbersToRight() {
 			}
 		});
 	} else {
-		if (jQuery('#noParens').attr('checked') == 'checked') {
+		if (jQuery('#noParens').filter(':checked').length) {
 			jQuery('.facetCountSupplyWrapper').each(function() {
 				jQuery(this).insertBefore(jQuery(this).prev('.filterCheckboxWrapper'));
 			});
@@ -41,7 +41,7 @@ jQuery('#noParens').click( function removeParens() {
 			//jQuery(this).html( jQuery(this).html().replace(")", "", "g") );
 		});
 	} else {
-		if (jQuery('#numOnRight').attr('checked') == 'checked') {
+		if (jQuery('#numOnRight').filter(':checked').length) {
 			// only put the beginning parens back. if there is no sibling after, put both parens back
 			jQuery('.facetCountSupplyWrapper').each(function() {
 				jQuery(this).find('span').html( jQuery(this).find('span').html().replace(/^([0-9,]+)/g, '($1') );
@@ -67,8 +67,32 @@ jQuery('#noParens').click( function removeParens() {
 
 jQuery('#chkbx').click( function addCheckbox() {
 	if (jQuery(this).filter(':checked').length) {
+		// Re-align the facet number titles to the right
+		jQuery('.facetNumberTitle .facetCountSupplyWrapper').css('margin-left','14px');
+		// Widen the left pane
+		jQuery('.facet-ui').width( jQuery('.facet-ui').width() + 14 );
+		
+		// Add the checkbox
+		jQuery('#facetsSupply .facets>li>div:last-child ul, #facetsBoth .facets>li>div:last-child ul, #facetsDemand .facets>li>div:last-child ul').each(function insert() {
+			var facet = jQuery(this).parent().attr('id'),
+			section = jQuery(this).parents().eq(4).attr('id').split('facets')[1];
+	
+			jQuery(this)
+			.children()
+			.each(function () {
+				jQuery(this).prepend('<div class="facetCheckbox"><input id="chkbx_'+facet+'_'+jQuery(this).find('.filterCheckboxWrapper').html().replace(/\s/g,"").replace(/(<b>)/g,"").replace(/(<\/b>)/g,"")+'_'+section+'" type="checkbox" /></div>');
+			});
+		});
+	
+		// Check the selected checkbox
+		jQuery('#AllFilters li.selected').find(':checkbox').attr('checked', 'checked');
+		
 	} else {
 		// remove checkboxes
 		jQuery('#AllFilters .facetCheckbox').remove();
+		// Re-align the facet number titles to the left
+		jQuery('.facetNumberTitle .facetCountSupplyWrapper').css('margin-left','0');
+		// Make the width of the left pane narrower
+		jQuery('.facet-ui').width( jQuery('.facet-ui').width() - 14 );
 	}
 });
