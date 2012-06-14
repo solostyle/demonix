6a6e4342f03ccb242995425c19195a655041c1f0
solostyle <spam@solostyle.net>
2012-06-14 14:57:22 -0500

    ellen's idea incorporated

--- summary_prototypes/1_summary_files/control-panel.js
+++ summary_prototypes/1_summary_files/control-panel.js
@@ -5,8 +5,7 @@ jQuery('body')
 .append('<ul></ul>')
 .find('ul')
 .append('<li><input type="checkbox" id="cpTwoLine"><label for="cpTwoLine">Two Lines</label></li>')
-.append('<li><input type="checkbox" id="cpLong"><label for="cpLong">Long Search</label></li>')
-.append('<li><input type="checkbox" id="cpSwap"><label for="cpSwap">Swap Text</label></li>');
+.append('<li><input type="checkbox" id="cpLong"><label for="cpLong">Long Search</label></li>');
 /*.append('<li><input type="checkbox" id="chkbx" checked="checked"><label for="chkbx">Checkbox</label></li>')
 .append('<li><input type="checkbox" id="wideMenu" checked="checked"><label for="wideMenu">Wide menu</label></li>')
 .end()
@@ -24,40 +23,16 @@ jQuery('body')
 
 jQuery('#cpTwoLine').bind('click', function() {
 	if (jQuery(this).filter(':checked').length) {
-		if (jQuery('#cpSwap').filter(':checked').length) {
-		} else {
-			$('#toolbar').css('height','46px');
-			$('#toolbar>div').css('height','46px');
-			$('#compLinkHeader,#exportHeader,#helpHeader').css('padding','14px 10px');
-			$('#compLinkHeader').css('padding','14px 10px 14px 20px');
-			$('#compLinkHeader').css('background-position','3px 13px');
-			$('#toolbarHeaderOuter').css('padding-top','5px');
-			$('#toolbar').find('h1').css('font-size','14px');
-			$('#toolbar').find('.headerJobTitle').css('font-size','16px');
-		}
-		$('<br>').insertAfter('.headerJobTitle');
+		$('<br>').insertAfter('#headerJobTitle');
 	} else {
-		$('#toolbar,#toolbar>div,#compLinkHeader,#exportHeader,#helpHeader,#compLinkHeader,#toolbarHeaderOuter,#headerJobTitle,#toolbar h1').removeAttr('style');
-		$('.headerJobTitle').next().remove('br');
+		$('#headerJobTitle').next().remove('br');
 	}
 });
 
 jQuery('#cpLong').bind('click', function() {
 	if (jQuery(this).filter(':checked').length) {
-		$('#toolbar').find('.headerJobTitle').html('"Web Developer" or "Java Developer" or "Software Engineer" or "Computer Systems Analyst" or "C++ Developer" or "Programmer"');
-		jQuery('#toolbarHeaderOuter').width('525px');
+		$('#headerJobTitle').html('"Web Developer" or "Java Developer" or "Software Engineer" or "Computer Systems Analyst" or "C++ Developer" or "Programmer"');
 	} else {
-		$('#toolbar').find('.headerJobTitle').html('"Web Developer"');
-	}
-});
-
-jQuery('#cpSwap').bind('click', function() {
-	if (jQuery(this).filter(':checked').length) {
-		Prototypes.saveHeader(jQuery('#toolbar').find('h1').html());
-		jQuery('#toolbar').find('h1').html('Overview').css({'font-size':'16px','color':'white'});
-		jQuery('#searchTerms').html(Prototypes.retrieveHeader()).show();
-	} else {
-		jQuery('#toolbar').find('h1').html(Prototypes.retrieveHeader());
-		jQuery('#searchTerms').hide();
+		$('#headerJobTitle').html('"Web Developer"');
 	}
 });
