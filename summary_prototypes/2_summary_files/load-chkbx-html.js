6d9628b809fb36c2c3a975d7c61028968a987f98
solostyle <spam@solostyle.net>
2012-06-15 14:27:52 -0500

    no hiring, custom supply

---
+++ summary_prototypes/2_summary_files/load-chkbx-html.js
@@ -0,0 +1,13 @@
+jQuery('#facetsSupply .facets>li>div:last-child ul, #facetsBoth .facets>li>div:last-child ul, #facetsDemand .facets>li>div:last-child ul').each(function insert() {
+	var facet = jQuery(this).parent().attr('id'),
+	section = jQuery(this).parents().eq(4).attr('id').split('facets')[1];
+	
+	jQuery(this)
+	.children()
+	.each(function () {
+		jQuery(this).prepend('<div class="facetCheckbox"><input id="chkbx_'+facet+'_'+jQuery(this).find('.filterCheckboxWrapper').html().replace(/\s/g,"").replace(/(<b>)/g,"").replace(/(<\/b>)/g,"")+'_'+section+'" type="checkbox" /></div>');
+	});
+});
+	
+// Check the selected checkbox
+jQuery('#AllFilters li.selected').find(':checkbox').attr('checked', 'checked');
